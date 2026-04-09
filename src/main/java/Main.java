2c16366e1b013db3b4fc9c8013c6c08a8b701992
deepbhagat123 <deepbhagatfootball8@gmail.com>
2026-04-09 16:27:32 +0530

    feat: add Product model with encapsulation, getters and setters

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,24 @@
+import com.smartcart.model.Product;
 public class Main {
+
+
+    public static void main(String[] args) {
+        Product tv = new Product();
+        tv.setName("Samsung TV");
+        tv.setPrice(10000);
+        tv.setStockQuantity(500);
+
+        Product machine = new Product();
+        machine.setName("symphony");
+        machine.setPrice(20000);
+        machine.setStockQuantity(700);
+
+
+
+
+        System.out.println(tv.getName() +" | price: " + (int)tv.getPrice());
+
+        System.out.println(machine.getName() + " | Price: " + machine.getPrice());
+
+    }
 }
---
+++ src/main/java/com/smartcart/model/Product.java
@@ -0,0 +1,18 @@
+package com.smartcart.model;
+
+public class Product {
+    private String name;
+    private double price;
+    private int stockQuantity;
+
+    // getter
+    public String getName() { return name;}
+    public double getPrice() { return price;}
+    public int getStockQuantity() { return stockQuantity;}
+
+
+    // setter
+    public void setName(String name) {this.name = name;}
+    public void setPrice(double price) { this.price = price;}
+    public void setStockQuantity(int stockQuantity) { this.stockQuantity=stockQuantity;}
+}
