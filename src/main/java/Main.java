11d60fdf9bf3eea9b19744c1d2937a074b657b88
deepbhagat123 <deepbhagatfootball8@gmail.com>
2026-04-12 15:39:24 +0530

    feat: add ArrayList with add, remove, get operations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,30 @@
 import com.smartcart.model.Electronics;
 import com.smartcart.model.Product;
 import com.smartcart.model.Grocery;
+
+import java.util.ArrayList;
 public class Main {
 
 
     public static void main(String[] args) {
-        Product tv = new Electronics("samsung TV", 10000,500,12);
 
+        ArrayList<Product> products = new ArrayList<>();
 
-        Product machine = new Electronics("Symphony",20000,400,24);
+        products.add(new Electronics("Samsung", 7000, 500,24));
+        products.add(new Electronics("Symphony TV",5000,400,36));
+        products.add(new Grocery("soviner",8000,400,"12-04-26"));
 
+        for(Product p : products){
+            System.out.println(p);
+        }
 
-        Grocery milk = new Grocery("Amul Milk", 60, 200, "2025-12-01");
-        System.out.println(milk);
+        System.out.println("Total Products " + products.size());
 
-        System.out.println(tv);
+        products.remove(0);
+        System.out.println("After removal " + products.size());
 
-        System.out.println(machine);
+        Product first = products.get(0);
+        System.out.println("first product | : " + first);
 
     }
 }
